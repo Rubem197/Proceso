99997ea164ff227676a3481bb3032bae027e22e9
ruben lindes roldan <ruben.lindes@iesnervion.es>
2022-09-28 09:49:31 +0200

    awa

--- src/ComprobarProcesoL.java
+++ src/ComprobarProcesoL.java
@@ -1,6 +1,22 @@
-public class ComprobarProcesoL {
-    public static void main(String[] args) {
+import java.io.IOException;
 
-        
+public class ComprobarProcesoL {
+    public static void main(String[] args) throws IOException {
+
+
+        ProcessBuilder pb = new ProcessBuilder(args);
+
+        pb.command("java", "ProcesoLento");
+
+        Process p = pb.start();
+        while (p.isAlive()) {
+            try {
+                System.out.println(p.isAlive());
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+
+            }
+        }
     }
 }
